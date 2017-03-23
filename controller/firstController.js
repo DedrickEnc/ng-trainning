7fc87777b24a04deb45a88360c7129825c3b22c4
DedrickEnc <kitamuka@gmail.com>
2017-03-23 12:29:22 +0100

    Add a first controller

---
+++ controller/firstController.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Dedrick Kitamuka on 23/03/2017.
+ */
+
+/**
+ * The controller is there to augment the scope, because we are using a controller for two reasons
+ * 1. Adding property to the scope
+ * 2. Adding behaviors to the scope
+ **/
